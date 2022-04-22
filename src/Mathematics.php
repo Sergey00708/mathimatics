a2088bad1dbdd6e00410da7cfaed6dfe5be9f8e9
Sergey <Fenix00708@gmail.com>
2022-04-22 19:48:12 +0300

    first commit

---
+++ src/Mathematics.php
@@ -0,0 +1,15 @@
+<?php
+namespace Mathimatics;
+
+class Mathematics
+{
+    public function sum($a, $b)
+    {
+        return $a + $b;
+    }
+
+    public function divide($a, $b)
+    {
+        return $a / $b;
+    }
+}
---
+++ src/Test/Hello.php
@@ -0,0 +1,10 @@
+<?php
+namespace Mathimatics\Test;
+
+class Hello
+{
+    public function seyHello()
+    {
+        return 'Hello World';
+    }
+}
